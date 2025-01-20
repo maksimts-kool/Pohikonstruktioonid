555705fa3f1b7e049edf1f1ffdc41252099330cf
opilane <opilane@HP-CZC2349HT0>
2025-01-20 11:26:12 +0200

    kalkulaator

--- Pohikonstruktioonid.py
+++ Pohikonstruktioonid.py
@@ -1,2 +1,35 @@
 print("Hello world","2")
 # separator, end
+
+# andmetüübid 
+# täisarv - int, ujukomaarv - float, sõne - str, tõeväärtused - bool
+# astendamine **
+a = 5
+aste = 2
+tulemus = a ** aste
+print(a,"**",aste,"=",tulemus)
+# Korrutamine *
+arv1 = int(input("Esimene korrutaja: "))
+arv2 = int(input("Teine korrutaja: "))
+tulemus = arv1 * arv2
+print(arv1,"*",arv2,"=",tulemus)
+# Jagamine / 
+arv1 = int(input("Esimene korrutaja: "))
+arv2 = int(input("Jagaja: "))
+tulemus = arv1 / arv2
+print(arv1,"/",arv2,"=",tulemus)
+# Liitmine +
+arv1 = int(input("Esimene korrutaja: "))
+arv2 = int(input("Teine korrutaja: "))
+tulemus = arv1 + arv2
+print(arv1,"+",arv2,"=",tulemus)
+# Lahutamine -
+arv1 = int(input("Esimene korrutaja: "))
+arv2 = int(input("Teine korrutaja: "))
+tulemus = arv1 - arv2
+print(arv1,"-",arv2,"=",tulemus)
+# jagamise täisarvulise osa leidmine //
+arv1 = int(input("Esimene korrutaja: "))
+arv2 = int(input("Teine korrutaja: "))
+tulemus = arv1 // arv2
+print(arv1,"//",arv2,"=",tulemus)
