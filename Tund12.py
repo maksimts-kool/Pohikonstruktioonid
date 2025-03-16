35bf82c3835a29eb3b8c061371915ccb6f52d6e3
Maksim Tsikvasvili <maksimtsitkool@gmail.com>
2025-03-16 16:25:13 +0200

    Update Tund12.py

--- Tund12.py
+++ Tund12.py
@@ -2,33 +2,50 @@ import random
 
 sõnad = ['python', 'programmeerimine', 'algoritm', 'arvuti', 'klaviatuur', 'hiir', 'monitor']
 
-
 randsõna = random.choice(sõnad)
 randsõnacount = len(randsõna)
 
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+WHITE = '\033[0m'
 
 kordus = 6
-print(f"Угадайте слово из {randsõnacount} букв. У вас {kordus} попыток.")
+print(f"Arvake sõna {randsõnacount} tähtedest. Teil on {kordus} proovid.")
 
 for j in range(kordus):
-    guess = input(f"Попытка {j + 1}: ").lower()
-    
-    if len(guess) != randsõnacount:
-        print(f"Пожалуйста, введите слово из {randsõnacount} букв.")
-        continue
-    
-    result = []
-    for i in range(len(guess)):
-        if guess[i] == randsõna[i]:
-            # if letter on right place green
-        elif guess[i] in randsõna:
-            # if letter on wrong place yellow
+    while True:
+        guess = input(f"Proov {j + 1}: ").lower()
+        
+        if len(guess) == randsõnacount:
+            break
         else:
-            # if no that letter white
+            print(f"Palun sisestage {randsõnacount} tähega sõna.")
     
+    colored_guess = []
+    yellowletter = []
+    
+    # Rohelised
+    for i in range(randsõnacount):
+        if guess[i] == randsõna[i]:
+            colored_guess.append(f"{GREEN}{guess[i]}{WHITE}")
+        else:
+            colored_guess.append(None)
+            yellowletter.append(randsõna[i])
+    
+    # Kollased ja Valged
+    for i in range(randsõnacount):
+        if colored_guess[i] == None:
+            if guess[i] in yellowletter:
+                colored_guess[i] = f"{YELLOW}{guess[i]}{WHITE}"
+                yellowletter.remove(guess[i])
+            else:
+                colored_guess[i] = f"{WHITE}{guess[i]}{WHITE}"
+    
+    # List > Sõne
+    print("".join(colored_guess))
     
     if guess == randsõna:
-        print("Поздравляем! Вы угадали слово!")
+        print("Palju õnne! Sa arvasid sõna ära!")
         break
 else:
-    print(f"К сожалению, вы не угадали слово. Загаданное слово было: {randsõna}")
+    print(f"Te ei arvanud sõna ära. Õige sõna oli: {randsõna}")
